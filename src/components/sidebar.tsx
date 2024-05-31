3b2cc9dfe1d28aa44722e4dab1acd12f703e907e
Bernardo Mendes <bernardomennndes@outlook.com>
2024-05-31 18:47:50 -0300

    feat: add sidebar component

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Ellipsis } from "lucide-react";
+import type { Routine } from "../models/routine";
+import { useRoutineStore } from "@/stores/routine-store";
+import { cn } from "@/lib/utils";
+
+interface RoutineProps {
+  routine: Routine;
+  selected: boolean;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+function RoutineItem(props: RoutineProps) {
+  const { routine, selected, onClick } = props;
+
+  return (
+    <button
+      className={cn(
+        "px-3 py-2 rounded-lg text-left inline-flex justify-between items-center hover:bg-muted group",
+        selected && "border border-border"
+      )}
+      onClick={onClick}
+    >
+      {routine.name}
+
+      <button className="hidden group-hover:block">
+        <Ellipsis className="text-muted-foreground" />
+      </button>
+    </button>
+  );
+}
+
+interface SidebarProps {
+  routine: Routine | undefined;
+}
+
+export function Sidebar(props: SidebarProps) {
+  const { routine: selectedRoutine } = props;
+
+  const routines = useRoutineStore((store) => store.routines);
+  const setRoutine = useRoutineStore((store) => store.setRoutine);
+
+  console.log(routines);
+
+  return (
+    <div className="w-[320px] py-8 px-6 border-r border-border flex flex-col gap-3">
+      <div className="text-sm font-semibold text-muted-foreground">
+        Rotinas Planejadas
+      </div>
+
+      <div className="flex flex-col gap-2">
+        {routines.map((routine) => (
+          <RoutineItem
+            key={routine.id}
+            routine={routine}
+            selected={routine.id === selectedRoutine?.id}
+            onClick={() => {
+              setRoutine(routine.id);
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
