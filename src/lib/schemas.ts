4fccc21ceb5d82b95ea197ab471bc38fbdf09ad2
Bernardo Mendes <bernardomennndes@outlook.com>
2024-05-31 18:46:11 -0300

    feat(task): add task schema

---
+++ src/lib/schemas.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const taskSchema = z.object({
+  name: z.string().min(10, {
+    message: "O nome da tarefa deve conter no mínimo 10 caracteres",
+  }),
+  description: z
+    .string()
+    .min(20, {
+      message: "A descrição da tarefa deve conter no mínimo 20 caracteres",
+    })
+    .optional(),
+  start: z
+    .number()
+    .min(0, { message: "O início da tarefa deve ser no mínimo as 0h" })
+    .max(23, { message: "O início da tarefa deve ser no máximo as 0h" }),
+  end: z
+    .number()
+    .min(1, { message: "O término da tarefa deve ser no mínimo as 1h" })
+    .max(24, { message: "O término da tarefa deve ser no máximo as 24h" }),
+});
