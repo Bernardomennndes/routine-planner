2ae5b36d86562ced3b199963b9cd4b8e490a19ce
Bernardo Mendes <bernardomennndes@outlook.com>
2024-06-01 13:22:15 -0300

    feat: change routine reference based on store structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Board } from "./components/board";
 import { useRoutineStore } from "./stores/routine-store";
 
 function App() {
-  const routine = useRoutineStore((store) => store.routine);
+  const routine = useRoutineStore(({ routineIndex, routines }) =>
+    routineIndex !== undefined ? routines[routineIndex] : undefined
+  );
 
   return (
     <main className="min-h-screen w-screen flex">
