e69f8e7823de1f8da4d1f1befffac4621bb3270c
Bernardo Mendes <bernardomennndes@outlook.com>
2024-06-01 13:19:22 -0300

    feat(routine store): add task insertion and deletion methods

--- src/stores/routine-store.ts
+++ src/stores/routine-store.ts
@@ -3,23 +3,63 @@ import type { Routine } from "@/models/routine";
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
-import { routines as mockedRoutines } from "@/data/routines";
+import { z } from "zod";
+import { taskSchema } from "@/lib/schemas";
 
 type RoutineStoreState = {
   routines: Routine[];
-  routine: Routine | undefined;
-  setRoutine: (id: string) => void;
+  routineIndex: number | undefined;
+  setRoutine: (routineIndex: number) => void;
+  addTask: (values: {
+    routineIndex: number;
+    day: number;
+    values: z.infer<typeof taskSchema>;
+  }) => void;
+  deleteTask: (values: {
+    routineIndex: number;
+    day: number;
+    taskIndex: number;
+  }) => void;
 };
 
 type PersistedState = Pick<RoutineStoreState, "routines">;
 
 export const useRoutineStore = create(
   persist<RoutineStoreState, [], [], PersistedState>(
-    (set, get) => ({
-      routines: mockedRoutines,
-      routine: undefined,
-      setRoutine: (id: string) =>
-        set({ routine: get().routines.find((routine) => routine.id === id) }),
+    (set) => ({
+      routines: [],
+      routineIndex: undefined,
+      setRoutine: (routineIndex: number) => set({ routineIndex }),
+      addTask: ({ routineIndex, day, values }) =>
+        set((state) => ({
+          routines: state.routines.map((routine, index) =>
+            index === routineIndex
+              ? {
+                  ...routine,
+                  schedule: {
+                    ...routine.schedule,
+                    [day]: [...routine.schedule[day], values],
+                  },
+                }
+              : routine
+          ),
+        })),
+      deleteTask: ({ routineIndex, day, taskIndex }) =>
+        set((state) => ({
+          routines: state.routines.map((routine, index) =>
+            index === routineIndex
+              ? {
+                  ...routine,
+                  schedule: {
+                    ...routine.schedule,
+                    [day]: routine.schedule[day].filter(
+                      (_, index) => index !== taskIndex
+                    ),
+                  },
+                }
+              : routine
+          ),
+        })),
     }),
     {
       name: "routine-storage",
