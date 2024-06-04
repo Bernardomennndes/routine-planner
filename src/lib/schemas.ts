8d80babab6b35e3eb9bbbcb34bbcbd7e871e1ba9
Bernardo Mendes <bernardomennndes@outlook.com>
2024-06-04 14:55:04 -0300

    fix: fix name field validation

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,7 +1,9 @@
 import { z } from "zod";
 
 export const taskSchema = z.object({
-  name: z.string({ message: "Campo obrigatório" }),
+  name: z
+    .string({ message: "Campo obrigatório" })
+    .min(1, { message: "Campo obrigatório" }),
   description: z
     .string()
     .min(20, {
@@ -11,7 +13,7 @@ export const taskSchema = z.object({
   start: z
     .number({ message: "Campo obrigatório" })
     .min(0, { message: "O início da tarefa deve ser no mínimo as 0h" })
-    .max(23, { message: "O início da tarefa deve ser no máximo as 0h" }),
+    .max(23, { message: "O início da tarefa deve ser no máximo as 23h" }),
   end: z
     .number({ message: "Campo obrigatório" })
     .min(1, { message: "O término da tarefa deve ser no mínimo as 1h" })
